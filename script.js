aed5f583ea57bdbef64b08c0aef4e8030e17314a
huynhquockhang131102 <khanghuynh.31231570190@st.ueh.edu.vn>
2025-05-15 11:01:59 +0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href="#"]').forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    alert('Chức năng này đang được phát triển!');
-  });
-});
