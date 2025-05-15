9e5e2c69b8d2e0df7a9f49971aade36f02ab05fb
huynhquockhang131102 <khanghuynh.31231570190@st.ueh.edu.vn>
2025-05-15 12:54:07 +0700

    Delete khang.js

--- khang.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    if (window.scrollY > 50) {
-        header.classList.add("bg-white", "shadow-lg");
-    } else {
-        header.classList.remove("bg-white", "shadow-lg");
-    }
-});
